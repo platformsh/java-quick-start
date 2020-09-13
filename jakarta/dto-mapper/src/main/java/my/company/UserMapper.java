facb340e22c3b629902f5eaab30a01321adf5515
otaviojava <otaviopolianasantana@gmail.com>
2020-09-13 08:14:46 -0300

    updates user mapper

--- jakarta/dto-mapper/src/main/java/my/company/UserMapper.java
+++ jakarta/dto-mapper/src/main/java/my/company/UserMapper.java
@@ -1,8 +1,35 @@
 package my.company;
 
 
+import org.javamoney.moneta.Money;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import javax.money.MonetaryAmount;
 
 @Mapper(componentModel = "cdi")
 public interface UserMapper {
+
+    @Mapping(source = "salary", target = "salary", qualifiedByName = "moneyString")
+    UserDTO toDTO(User user);
+
+    @Mapping(source = "salary", target = "salary", qualifiedByName = "money")
+    User toEntity(UserDTO dto);
+
+    @Named("money")
+    static MonetaryAmount money(String money) {
+        if (money == null) {
+            return null;
+        }
+        return Money.parse(money);
+    }
+
+    @Named("moneyString")
+    static String moneyString(MonetaryAmount money) {
+        if (money == null) {
+            return null;
+        }
+        return money.toString();
+    }
 }
