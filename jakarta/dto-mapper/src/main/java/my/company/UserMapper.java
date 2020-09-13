f947a24f88018d08de3cc1789e2c8a7b2372c094
otaviojava <otaviopolianasantana@gmail.com>
2020-09-13 08:38:33 -0300

    updates the mapper

--- jakarta/dto-mapper/src/main/java/my/company/UserMapper.java
+++ jakarta/dto-mapper/src/main/java/my/company/UserMapper.java
@@ -7,14 +7,17 @@ import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 import javax.money.MonetaryAmount;
+import java.time.LocalDate;
 
 @Mapper(componentModel = "cdi")
 public interface UserMapper {
 
     @Mapping(source = "salary", target = "salary", qualifiedByName = "moneyString")
+    @Mapping(source = "birthday", target = "birthday", qualifiedByName = "localDateString")
     UserDTO toDTO(User user);
 
     @Mapping(source = "salary", target = "salary", qualifiedByName = "money")
+    @Mapping(source = "birthday", target = "birthday", qualifiedByName = "localDate")
     User toEntity(UserDTO dto);
 
     @Named("money")
@@ -25,11 +28,27 @@ public interface UserMapper {
         return Money.parse(money);
     }
 
+    @Named("localDate")
+    static LocalDate localDate(String date) {
+        if (date == null) {
+            return null;
+        }
+        return LocalDate.parse(date);
+    }
+
     @Named("moneyString")
     static String moneyString(MonetaryAmount money) {
         if (money == null) {
             return null;
         }
         return money.toString();
+    }
+
+    @Named("localDateString")
+    static String localDateString(LocalDate date) {
+        if (date == null) {
+            return null;
+        }
+        return date.toString();
     }
 }
--- jakarta/dto-mapper/src/test/java/my/company/MapperTest.java
+++ jakarta/dto-mapper/src/test/java/my/company/MapperTest.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ModelMapperTest {
+public class MapperTest {
 
     private UserMapper mapper;
 
